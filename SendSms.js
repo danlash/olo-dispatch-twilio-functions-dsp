9a2ce621b0ff2d3f94e55f72e3f6f2a828a97769
Dan Lash <dan@olo.com>
2018-08-21 20:47:41 -0400

    website for sending sms

---
+++ SendSms.js
@@ -0,0 +1,17 @@
+exports.handler = function(context, event, callback) {
+	const client = context.getTwilioClient();
+    const body = 'Start driving by sending a text command:\n"Drive  <zip>,<zip>  <fee>"' 
+
+    client.messages.create({
+        body: body,
+        to: event.phoneNumber,
+        from: context.TWILIO_PHONE_NUMBER
+    })
+    .then(() => {
+        callback(null, '');
+    })
+    .catch(err => {
+        console.log('ERR ' + err);
+        return callback(err);
+    });
+};
